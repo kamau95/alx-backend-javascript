be294787180b1b7871829c16df47c82a46943f76
kamau95 <linuskamau586@gmail.com>
2024-07-31 11:03:24 +0300

    for loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,16 @@
-export default function appendToEachArrayValue(array, appendString) {
+/*  export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
   for (const [index, value] of array.entries()) {
     newArray[index] = `${appendString}${value}`;
   }
   return newArray;
+}
+*/
+export default function appendToEachArrayValue(array, appendString) {
+  const arrayEnd = [];
+  for (const idx of array) {
+    arrayEnd.push(`${appendString}${idx}`);
+  }
+
+  return arrayEnd;
 }

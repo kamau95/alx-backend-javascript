90604abeb0046b137d2a80676013d65544656ab3
kamau95 <linuskamau586@gmail.com>
2024-08-02 00:44:55 +0300

    final user file

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup([firstName, lastName, fileName]) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const prom1 = signUpUser(firstName, lastName);
   const prom2 = uploadPhoto(fileName);
-  return Promise.allSettled(prom1, prom2)
+  return Promise.allSettled([prom1, prom2])
     .then((values) => {
       const result = [];
       values.forEach((element) => {
-        if (element.status === 'fullfilled') {
+        if (element.status === 'fulfilled') {
           result.push({ status: element.status, value: element.value });
         } else {
           result.push({ status: element.status, value: `${element.reason}` });

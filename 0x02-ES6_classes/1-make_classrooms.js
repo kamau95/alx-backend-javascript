b7f9fedadf179b67fb54aa85d50e60ac82cbf9ba
kamau95 <linuskamau586@gmail.com>
2024-08-05 11:28:34 +0300

    classes

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const obj1 = new ClassRoom(19);
+  const obj2 = new ClassRoom(20);
+  const obj3 = new ClassRoom(34);
+  const classes = [obj1, obj2, obj3];
+  return classes;
+  }

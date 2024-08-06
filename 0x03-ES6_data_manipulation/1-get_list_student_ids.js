b3debf3034e7464dc914013a5d6c942eb27e9003
kamau95 <linuskamau586@gmail.com>
2024-08-06 16:45:58 +0300

    finn

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,4 @@
+/*
 export default function getListStudentIds(lists) {
   const arr = [];
   if (!Array.isArray(lists)) {
@@ -6,3 +7,12 @@ export default function getListStudentIds(lists) {
   lists.forEach((element) => arr.push(element.id));
   return arr;
 }
+*/
+const getListStudentIds = (myArray) => {
+  if (!Array.isArray(myArray)) {
+    return [];
+  }
+  return myArray.map((studentId) => studentId.id);
+};
+
+export default getListStudentIds;

f9ac0c95bc756886fe41ca18b6440abc4fe2be96
kamau95 <linuskamau586@gmail.com>
2024-08-06 16:41:24 +0300

    manipulating arrays

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,15 @@
+/*
 export default function getListStudents() {
   const obj1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
   const obj2 = { id: 2, firstName: 'James', location: 'Columbia' };
   const obj3 = { id: 3, firstName: 'Serena', location: 'San Francisco' };
   return [obj1, obj2, obj3];
 }
+*/
+const getListStudents = () => [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+];
+
+export default getListStudents;

4124b81db1d36bf237de4d8af311953b896a4101
kamau95 <linuskamau586@gmail.com>
2024-08-07 15:34:28 +0300

    sweet sweet typed arrays-

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(myset, start) {
+  if (start === undefined || start.length === 0) {
+    return '';
+  }
   return Array.from(myset)
     .filter((value) => value.startsWith(start))
     .map((value) => value.slice(start.length))

f730e117dc54b452b1b8f32d48af42ade24ddf03
kamau95 <linuskamau586@gmail.com>
2024-09-17 09:29:48 +0300

    cml arguments

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+// create interface for the input and output
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('what is your name?', (name) => {
+  console.log(`Your name is: ${name}`);
+  console.log('This important software is now closing');
+  rl.close();
+});

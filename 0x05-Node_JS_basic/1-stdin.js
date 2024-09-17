aa4b776941d739b0f9f34926370671104b307b64
kamau95 <linuskamau586@gmail.com>
2024-09-17 09:35:27 +0300

    user input fetch

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question('what is your name?', (name) => {
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
   console.log('This important software is now closing');
   rl.close();

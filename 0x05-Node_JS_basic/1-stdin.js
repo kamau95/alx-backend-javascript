20ce886810423d2224f2d607f52a1894e3bdeb46
kamau95 <linuskamau586@gmail.com>
2024-09-17 21:23:51 +0300

    process.stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,3 +1,4 @@
+/*
 const readline = require('readline');
 // create interface for the input and output
 const rl = readline.createInterface({
@@ -9,4 +10,14 @@ rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
   console.log('This important software is now closing');
   rl.close();
+});
+*/
+
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (data) => {
+  // const message = data.toString().trim();
+  const message = data;
+  console.log(`Your name is: ${message}`);
+  console.log('This important software is now closing');
+  process.exit();
 });
